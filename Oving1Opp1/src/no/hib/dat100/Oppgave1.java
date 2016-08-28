1118282cd90930c6dc55051a6ee71e468851b9e7
Robin <Robin@Robin-PC.lan>
2016-08-28 16:36:08 +0200

    Øving 1 - Oppgave 1

---
+++ Oving1Opp1/src/no/hib/dat100/Oppgave1.java
@@ -0,0 +1,22 @@
+package no.hib.dat100;
+
+public class Oppgave1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Rett på Java");
+		
+	}
+	
+}
+
+/* Oppgave 1
+ * a) Fjern ordet static.
+ * -	Kan ikke kjøres.
+ * b) Fjern ordet void.
+ * -	Kan ikke kjøres
+ * c) Skriv oppgave1 i stedet for Oppgave1.
+ * -	Kan ikke kjøres (finner ikke main class).
+ * d) Fjern den siste krøllparentesen.
+ * -	Kan ikke kjøres.
+ */
