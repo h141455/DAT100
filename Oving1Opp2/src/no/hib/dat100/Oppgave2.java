34dce6f5ea5f4c7a7a1caf7c3dc52dbc95cc7f65
Robin <Robin@Robin-PC.lan>
2016-08-28 16:40:09 +0200

    Øving 1 - Oppgave 2

---
+++ Oving1Opp2/src/no/hib/dat100/Oppgave2.java
@@ -0,0 +1,14 @@
+package no.hib.dat100;
+
+public class Oppgave2 {
+
+	public static void main(String[] args) {
+		System.out.println("    a");
+		System.out.println("   a a");
+		System.out.println("  a b a");
+		System.out.println(" a c c a");
+		System.out.println("a d f d a");
+
+	}
+
+}
