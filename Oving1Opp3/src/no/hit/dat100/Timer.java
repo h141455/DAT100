313e88ca184a5deceba927ca997d4a87a164ccec
Robin <Robin@Robin-PC.lan>
2016-08-28 17:26:01 +0200

    Øving 1 - Oppgave 3

---
+++ Oving1Opp3/src/no/hit/dat100/Timer.java
@@ -0,0 +1,29 @@
+package no.hit.dat100;
+
+import static javax.swing.JOptionPane.*;
+
+import javax.swing.JOptionPane;
+
+
+public class Timer {
+	
+	static String message = "Oppgi hvor mange sekunder";
+	static String title = "Oppgave 3";
+
+	public static void main(String[] args) {
+		// To show dialog box.
+		String tallTxt=showInputDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+		int brukerSekunder = Integer.parseInt(tallTxt);
+		
+		// Calculating time.
+		int antallTimer = brukerSekunder / 60;
+		int antallMinutter = brukerSekunder % 60;
+		int antallSekunder = brukerSekunder % 60;
+		
+		// Formating the answer and displaying the result in a dialog box.
+		String resultat = (antallTimer + " timer, " + antallMinutter + " minutter, " + antallSekunder + " sekunder.");
+		showMessageDialog(null, resultat, title, JOptionPane.INFORMATION_MESSAGE);
+
+	}
+
+}
